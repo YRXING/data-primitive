41657fc23f6c2573db3aca7ca609abb8e97227ff
xing <y1486170457@qq.com>
2022-03-08 19:21:55 +0800

    define supplier interface

---
+++ pkg/supplier/types.go
@@ -0,0 +1,28 @@
+package supplier
+
+const (
+	// It refers to the prepayment order, and the buyer can pledge the future cargo right.
+	ADVANCE_ORDER  = "advance-order"
+
+	// It refers to account receivable order, and the supplier can pledge the documents
+	// of accounts receivable orders.
+	ACCOUNTRECEIVABLE_ORDER = "account-receivable-order"
+
+	// It refers to financing warehouse order, and the supplier can pledge the inventory.
+	FINACINGWAREHOUSE_ORDER = "financing-warehouse-order"
+)
+
+type Order struct {
+	// There are three different order types, which represent different financing
+	// modes, so as to match different linkage paths.
+	OrderType string `json:"order_type"`
+
+	OrderNums int `json:"order_nums"`
+
+	OrderCount int `json:"order_count"`
+}
+
+type Capital struct {
+	BankName string `json:"bank_name"`
+	Num	int `json:"num"`
+}
---
+++ pkg/types/supplier.go
@@ -0,0 +1,13 @@
+package types
+
+import (
+	"context"
+	"github.com/YRXING/data-primitive/agent"
+)
+type supplier struct {
+
+}
+
+func (s *supplier) Interact(context.Context, *agent.Packet) (*agent.Packet, error)  {
+
+}
